99f3e00e89acd4eaabab048ae27158e6913e0d2b
MarinaB77777 <ramotaugu@gmail.com>
2025-05-23 21:37:36 -0600

    Update start.py

--- backend/start.py
+++ backend/start.py
@@ -1,4 +1,5 @@
 import uvicorn
+from app.init_db import *  # Импортируем init_db.py, чтобы create_all выполнился
 
 if __name__ == "__main__":
     uvicorn.run("app.main:app", host="0.0.0.0", port=10000, reload=False)

d0d58d3c12ba9e68630ec35b250c158b5ae97cfd
MarinaB77777 <ramotaugu@gmail.com>
2025-06-04 18:30:47 -0600

    Update login.py

--- backend/app/api/login.py
+++ backend/app/api/login.py
@@ -1,17 +1,23 @@
 from fastapi import APIRouter, HTTPException, Depends
 from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
 from datetime import timedelta
-from app.services.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES, fake_users_db
+from app.db import get_db
+from app.models.user import User
+from app.services.auth import create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
+from passlib.hash import bcrypt
 
 router = APIRouter()
 
 @router.post("/login")
-def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user = fake_users_db.get(form_data.username)
-    if not user:
-        raise HTTPException(status_code=400, detail="Invalid phone")
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.name == form_data.username).first()
+
+    if not user or not bcrypt.verify(form_data.password, user.hashed_password):
+        raise HTTPException(status_code=400, detail="Неверное имя пользователя или пароль")
+
     access_token = create_access_token(
-        data={"sub": user["phone"]},
-        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES),
+        data={"sub": str(user.id)},
+        expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
     return {"access_token": access_token, "token_type": "bearer"}

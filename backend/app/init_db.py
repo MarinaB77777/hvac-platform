d26b77f7b95341a13099dda9e29bf1fd3b8f7cee
MarinaB77777 <ramotaugu@gmail.com>
2025-06-06 11:20:35 -0600

    Update init_db.py

--- backend/app/init_db.py
+++ backend/app/init_db.py
@@ -1,5 +1,5 @@
 from app.db import Base, engine
-from app.models import user, order, warehouse, material_request
+from app.models import user, order, warehouse, material_request, material  # ✅ добавлен material
 
 print("Создание таблиц...")
 Base.metadata.create_all(bind=engine)

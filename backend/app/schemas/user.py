6fad1512b2696e5377cfb2a32975b19039ddbcfa
MarinaB77777 <ramotaugu@gmail.com>
2025-06-10 17:51:26 -0600

    Update user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -5,7 +5,11 @@ class UserCreate(BaseModel):
     name: str
     phone: str
     password: str
-    role: str 
+    role: str
+    location: Optional[str] = None
+    qualification: Optional[str] = None
+    rate: Optional[int] = None
+    status: Optional[str] = None 
 
 class UserUpdate(BaseModel):
     name: Optional[str] = None

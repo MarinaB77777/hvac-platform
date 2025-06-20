71cb453813a784f40556fa77335d807d5cf02530
MarinaB77777 <ramotaugu@gmail.com>
2025-06-19 19:35:50 -0600

    Update material.py

--- backend/app/models/material.py
+++ backend/app/models/material.py
@@ -5,6 +5,13 @@ from datetime import datetime
 class Material(Base):
     __tablename__ = "materials"
 
+materials_db = [
+    {"id": 1, "name": "Фреон", "brand": "R410", "unit_price": 15},
+    {"id": 2, "name": "Компрессор", "brand": "Hitachi", "unit_price": 120},
+    {"id": 3, "name": "Фильтр", "brand": "Panasonic", "unit_price": 40},
+    {"id": 4, "name": "Термостат", "brand": "Danfoss", "unit_price": 60}
+]
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     brand = Column(String)

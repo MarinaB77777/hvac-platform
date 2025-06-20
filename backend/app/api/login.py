a35c277ab0927e9eb4f7bc9b896bfd3db197e417
MarinaB77777 <ramotaugu@gmail.com>
2025-06-20 17:18:14 -0600

    Update login.py

--- backend/app/api/login.py
+++ backend/app/api/login.py
@@ -21,4 +21,14 @@ def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depend
         data={"sub": str(user.id)},
         expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     )
-    return {"access_token": access_token, "token_type": "bearer"}
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user": {
+            "id": user.id,
+            "name": user.name,
+            "phone": user.phone,
+            "role": user.role
+        }
+    }
+    

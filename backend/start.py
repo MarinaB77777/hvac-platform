2be7bd98dfa8b6899b8f8b8df82c46302d25bdc9
MarinaB77777 <ramotaugu@gmail.com>
2025-06-26 01:27:03 -0600

    Update start.py

--- backend/start.py
+++ backend/start.py
@@ -1,17 +1,5 @@
 import os
 import uvicorn
-import shutil
-
-# ✅ Просто проверяем путь
-tesseract_path = shutil.which("tesseract")
-print(f"🔍 Проверка tesseract path: {tesseract_path}")
-
-if not tesseract_path:
-    print("❗️tesseract не найден в PATH, указываем вручную")
-    os.environ["TESSERACT_CMD"] = "/usr/bin/tesseract"
-else:
-    print(f"✅ Используется tesseract: {tesseract_path}")
-    os.environ["TESSERACT_CMD"] = tesseract_path
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))

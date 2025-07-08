206e9deda5b567b823f921c420c8887985b7abab
MarinaB77777 <ramotaugu@gmail.com>
2025-07-08 13:27:38 -0600

    Update material_request.py

--- backend/app/models/material_request.py
+++ backend/app/models/material_request.py
@@ -5,7 +5,7 @@ from app.db import Base
 class MaterialRequest(Base):
     __tablename__ = "material_requests"
 
-id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     material_id = Column(Integer, ForeignKey("materials.id"), nullable=False)
     order_id = Column(Integer, nullable=True)
     hvac_id = Column(Integer, nullable=False)

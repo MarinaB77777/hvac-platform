2b5d195057bafb7d8b5517be060e8b9b2b28c85f
MarinaB77777 <ramotaugu@gmail.com>
2025-09-10 12:18:12 -0600

    Create organization.py

---
+++ backend/app/models/organization.py
@@ -0,0 +1,10 @@
+# models/organization.py
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Organization(Base):
+    __tablename__ = "organizations"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True, nullable=False)
+    description = Column(String, nullable=True)

c012af3889197783959073af153fef917f2dcdd8
MarinaB77777 <ramotaugu@gmail.com>
2025-09-10 14:02:00 -0600

    Update organization.py

--- backend/app/api/organization.py
+++ backend/app/api/organization.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.schemas.organization import OrganizationCreate, OrganizationOut, OrganizationUpdate
 from app.models.organization import Organization
-from app.dependencies import get_db
+from app.db import get_db
 
 router = APIRouter()
 

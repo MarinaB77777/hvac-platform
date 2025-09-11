b4cf098e79648731c3672fc84f21339f6a95edd6
MarinaB77777 <ramotaugu@gmail.com>
2025-09-10 18:01:39 -0600

    Update organization.py

--- backend/app/models/organization.py
+++ backend/app/models/organization.py
@@ -5,10 +5,13 @@ from app.db import Base
 class Organization(Base):
     __tablename__ = "organization"
 
-    id = Column(Integer, primary_key=True, index=True)
+     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
     description = Column(String, nullable=True)
     website = Column(String, nullable=True)
     address = Column(String, nullable=True)
-    phone = Column(String, nullable=True)
+    phone = Column(String, unique=True, nullable=False)
     email = Column(String, nullable=True)
+    
+    password_hash = Column(String, nullable=False)
+    role = Column(String, default="organization")

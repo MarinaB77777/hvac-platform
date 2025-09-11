04fe536a9fb7cd847c626442d8cc210758ee4901
MarinaB77777 <ramotaugu@gmail.com>
2025-09-11 00:00:39 -0600

    Update user.py

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from sqlalchemy import Column, Integer, String, Enum as SQLEnum, Boolean, DateTime, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Enum as SQLEnum, Boolean, DateTime, Float, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from app.db import Base
 

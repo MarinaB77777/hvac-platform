09b15aadd78f1e27b62b72db1fc86af2725163a5
MarinaB77777 <ramotaugu@gmail.com>
2025-09-10 18:06:42 -0600

    Update organization.py

--- backend/app/models/organization.py
+++ backend/app/models/organization.py
@@ -3,15 +3,14 @@ from sqlalchemy import Column, Integer, String
 from app.db import Base
 
 class Organization(Base):
-    __tablename__ = "organization"
+    __tablename__ = "organization"  # 👈 ОБЯЗАТЕЛЬНО с двумя подчёркиваниями
 
-     id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True, nullable=False)
     description = Column(String, nullable=True)
     website = Column(String, nullable=True)
     address = Column(String, nullable=True)
     phone = Column(String, unique=True, nullable=False)
     email = Column(String, nullable=True)
-    
     password_hash = Column(String, nullable=False)
     role = Column(String, default="organization")

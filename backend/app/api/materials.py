b8bf8dcf058cbb03af6a7e4684e4d396804d72e5
MarinaB77777 <ramotaugu@gmail.com>
2025-12-10 22:23:20 -0800

    Update materials.py

--- backend/app/api/materials.py
+++ backend/app/api/materials.py
@@ -7,10 +7,12 @@ from app.models.material import Material
 from app.schemas.material import MaterialOut, MaterialCreate
 
 router = APIRouter(prefix="/materials", tags=["materials"])
+
+
 @router.post("/", response_model=MaterialOut, status_code=201)
 def create_material(
     material: MaterialCreate,
-    db: Session = Depends(get_db),
+    db: Session = Depends(get_db)
 ):
     db_material = Material(**material.model_dump())
     db.add(db_material)
@@ -18,22 +20,28 @@ def create_material(
     db.refresh(db_material)
     return db_material
 
+
 @router.get("/", response_model=List[MaterialOut])
 def get_all_materials(
     db: Session = Depends(get_db),
-    brand: Optional[str] = Query(None),
-    name: Optional[str] = Query(None),
-    organization: Optional[str] = Query(None),
-    sort_by: Optional[str] = Query(None)
+    brand: Optional[str] = Query(None, description="Фильтр по бренду"),
+    name: Optional[str] = Query(None, description="Поиск по названию (включает подстроку)"),
+    sort_by: Optional[str] = Query(None, description="Сортировка: 'stock' или 'price'")
+
 ):
     query = db.query(Material)
 
+
     if brand:
         query = query.filter(Material.brand == brand)
+
     if name:
         query = query.filter(Material.name.ilike(f"%{name}%"))
+
+
     if sort_by == "stock":
         query = query.order_by(Material.stock.desc())
-    if organization:
-        query = query.filter(Material.organization == organization)
+    elif sort_by == "price":
+        query = query.order_by(Material.price_usd.asc())
+
     return query.all()

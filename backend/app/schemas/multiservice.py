b0f091849b6dfee903f5bf4cecf37126339911b0
MarinaB77777 <ramotaugu@gmail.com>
2026-02-06 09:39:39 -0800

    Create multiservice.py

---
+++ backend/app/schemas/multiservice.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+class MultiServiceCreate(BaseModel):
+    title: str
+    details: Optional[str] = None
+
+    road_tariff: Optional[int] = None
+    diagnostic_price: Optional[int] = None
+    materials_default: Optional[int] = None
+    base_price: Optional[int] = None
+
+class MultiServiceOut(BaseModel):
+    id: int
+    multiservice_code: str
+    title: str
+    details: Optional[str] = None
+
+    road_tariff: Optional[int] = None
+    diagnostic_price: Optional[int] = None
+    materials_default: Optional[int] = None
+    base_price: Optional[int] = None
+
+    created_by_user_id: Optional[int] = None
+    is_used: bool
+
+    created_at: datetime
+    updated_at: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
